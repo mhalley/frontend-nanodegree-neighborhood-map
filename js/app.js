b1c2172eaba01eff1d7d9319d0a28bd1b386f9e1
Marguerite Halley <marguerite.e.halley@gmail.com>
2016-03-03 13:13:39 -0600

    Chore: Add notes on what needs to appear in HTML and what needs to happen in JavaScript

---
+++ js/app.js
@@ -0,0 +1,21 @@
+//uses Knockout
+
+//Model
+//	Call Google API for list of locations
+//	Call Third party location info?
+
+//View
+//	Display map
+//	Display pins on map
+//	Display pop-up box of information about location
+//	Display search box
+//	Display list of locations
+//	Display newly sorted list of locations
+//	Display error message when problem
+
+
+//Octopus
+//	Tell View about locations
+//	Request additional information from 3rd party
+//	Sort list based on keyword
+//	Tell map which location to display?
